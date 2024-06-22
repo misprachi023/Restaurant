51009e6b29c8195bbb7ebfc6e15ff1f07cbe146f
misprachi023 <prachimishra23123@gmail.com>
2024-06-22 20:34:36 +0530

    add

--- Backend/app.js
+++ Backend/app.js
@@ -10,7 +10,7 @@ dotenv.config({ path: "./config/config.env" });
 
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL],
+    origin: [process.env.FRONTEND_URL, "https://restaurant-beta-livid.vercel.app"],
     methods: ["POST"],
     credentials: true,
   })

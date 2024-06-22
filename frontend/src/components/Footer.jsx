2a317b2ca69f7507400f1cac3005f1b8b9b35582
misprachi023 <prachimishra23123@gmail.com>
2024-06-22 15:42:13 +0530

    add frontend part code

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">Prachi 😋</div>
+          <div className="right">
+            <p>Bhopal, Madhya Pradesh</p>
+            <p>Open: 05:00 PM - 12:00 AM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By Prachi Mihsra ❤️ </p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By Prachi Mishra.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
